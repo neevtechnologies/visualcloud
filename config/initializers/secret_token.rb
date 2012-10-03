2197ea22c3330e17c47aa7e1cf12d2dd07a9b8a4
Emil S <emil.soman@gmail.com>
2012-10-03 14:15:30 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisualCloud::Application.config.secret_token = '6cba7d28b43d04223fb986eb3aa17fb6d3b5c84c76e2a38515f7e69b5c5d0c6b0762c1053adf4e2516f826e0aa23997a7c8a108cd3111f4d3959266b19f3faa0'
