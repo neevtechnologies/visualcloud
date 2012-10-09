bb3f9bdede2083402d2a8351353183791db46142
Emil S <emil.soman@gmail.com>
2012-10-09 19:12:23 +0530

    Amazon icon pack added

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ puts 'New user created: ' << user.name
 user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user2.name
 user.add_role :admin
+
+ResourceType.create(name: 'EC2', ami_id: "ami-d821afe8", small_icon: "amazon/AWS_Simple_Icons_Compute_Amazon_EC2.svg", large_icon: "amazon/AWS_Simple_Icons_Compute_Amazon_EC2.svg")
+ResourceType.create(name: 'RDS', ami_id: nil, small_icon: "amazon/AWS_Simple_Icons_Database_Amazon_RDS.svg", large_icon: "amazon/AWS_Simple_Icons_Database_Amazon_RDS.svg")
