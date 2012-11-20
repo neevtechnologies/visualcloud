70d27176263f751b283802e5dcf307ec3dad9255
Asit <asit@neevtech.com>
2012-11-20 17:55:26 +0530

    added default status image

--- app/helpers/environments_helper.rb
+++ app/helpers/environments_helper.rb
@@ -1,5 +1,5 @@
 module EnvironmentsHelper
   def status_image(status)
-    image_tag("#{status}.gif", title: "#{status}", rel: 'tooltip') if status.present?
+    status.present? ? image_tag("#{status}.gif", title: "#{status}", rel: 'tooltip') : image_tag("default_status.gif", title: "Not yet Provisioned!", rel: 'tooltip')
   end
 end
