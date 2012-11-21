a95f48efbff69098505b4239ea9bde0e192f7f9c
Asit <asit@neevtech.com>
2012-11-21 20:59:43 +0530

    Status Image Patch

--- app/controllers/environments_controller.rb
+++ app/controllers/environments_controller.rb
@@ -176,8 +176,8 @@ class EnvironmentsController < ApplicationController
   # Get the status of the Stack
   def status
     unless params[:id].blank?
-      environment = Environment.find(params[:id])
-      @status = environment.status(current_user.aws_access_key, current_user.aws_secret_key)
+      @environment = Environment.find(params[:id])
+      @status = @environment.status(current_user.aws_access_key, current_user.aws_secret_key)
       respond_to do |format|
         format.js
       end
