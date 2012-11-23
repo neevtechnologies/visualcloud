373963b5ec9e01a4e80e06d6dc42fbdabd96fe38
Emil S <emil.soman@gmail.com>
2012-11-23 20:23:24 +0530

    Missed commits

---
+++ lib/manage_nodes.rb
@@ -0,0 +1,19 @@
+module ManageNodes
+ def delete_client(client)
+  begin
+    system("knife client delete #{client} -y") if client.present? && client.to_i != 0  
+  rescue => e
+    puts "ERROR: Exception deleting client #{client}"
+    puts e.backtrace
+  end
+ end
+
+ def delete_node(node)
+  begin
+    system("knife node delete #{node} -y") if node.present? && node.to_i != 0
+  rescue => e
+    puts "ERROR: Exception deleting node #{node}"
+    puts e.backtrace
+  end
+ end
+end
--- lib/server_meta_data.rb
+++ lib/server_meta_data.rb
@@ -3,34 +3,38 @@ module ServerMetaData
 
   def update_node_data_bag(obj)
     begin
-      # Create a temporary json file
       file = File.open("/tmp/nodes.json","w+") 
       file.puts nodes_json(obj)
       file.close
-      # Create or update a data bag content
       system("knife data bag from file nodes /tmp/nodes.json")
     rescue => e
       puts "Error modifying Databag nodes for #{obj.id}! #{e.inspect}"
     ensure
-      # Ensure deletion of temporary file
       File.delete(file)
     end
   end
 
   def update_project_data_bag(obj, options={})
     begin
-      # Create a temporary json file
       file = File.open("/tmp/projects.json","w+") 
       file.puts projects_json(obj, options)
       file.close
-      # Create or update a data bag content
       system("knife data bag from file projects /tmp/projects.json")
     rescue => e
       puts "Error modifying Databag projects for #{obj.id}! #{e.inspect}"
       puts e.backtrace
     ensure
-      # Ensure deletion of temporary file
       File.delete(file)
     end
   end
+
+  def delete_data_bag_item(data_bag_name, obj_id)
+    begin
+      system("knife data bag delete #{data_bag_name} #{obj_id} -y")
+    rescue => e
+      puts "Error deleting Databag #{data_bag_name} for #{obj_id}! #{e.inspect}"
+      puts e.backtrace
+    end
+  end
+
 end
