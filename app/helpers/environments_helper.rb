67a100b6396db57ad413ba731d863c41f83f8059
Emil S <emil.soman@gmail.com>
2012-11-25 03:43:42 +0530

    Changing status icons

--- app/helpers/environments_helper.rb
+++ app/helpers/environments_helper.rb
@@ -1,9 +1,9 @@
 module EnvironmentsHelper
   def status_image(status)
     if (["CREATE_COMPLETE","CREATE_IN_PROGRESS"].include?status)
-      {"img_src" => image_path("#{status}.gif"),"img_title" => "#{status}"}
+      {"img_src" => image_path("#{status}.png"),"img_title" => "#{status}"}
     else
-      {"img_src" => image_path("default_status.gif"),"img_title" => "Not yet provisioned!"}
+      {"img_src" => image_path("default_status.png"),"img_title" => "Not yet provisioned!"}
     end
   end
 end
