15aea8d9803b7b07db9a925330753b559c68c9d7
Siva Prasad <sivaprasad@neevtech.com>
2012-12-17 11:28:13 +0530

    Removed unnecessary code in connector.js

--- app/assets/javascripts/environment/connector.js
+++ app/assets/javascripts/environment/connector.js
@@ -1,36 +1,10 @@
-//BELOW TO BE REMOVED
-
-//Makes the element a source for connections
-function makeSource(element){
-  var sourceEndPointAttributes = {
-    anchor: "BottomLeft",
-    endpoint: ["Dot", {radius: 7}],
-    connector:[ "Bezier", { curviness:50 }],
-    isSource: true,
-    maxConnections: -1
-  };
-  jsPlumb.addEndpoint(element, sourceEndPointAttributes);
-};
-
-//Makes the element a target for connections
-function makeTarget(element){
-  var targetEndPointAttributes = {
-    anchor: "TopLeft",
-    endpoint: ["Dot", {radius: 7}],
-    isTarget: true
-  };
-  jsPlumb.addEndpoint(element, targetEndPointAttributes);
-};
-
-//ABOVE TO BE REMOVED
-
 //Makes the element a source as well as target for connections
 function makeSourceAndTarget(element , parents_list){
   var sourceAndTargetEndPointAttributes = {
     anchors: ["BottomLeft","TopLeft","TopCenter","BottomCenter"],
     endpoint: ["Dot", {radius: 4}],
     connectorStyle: { lineWidth:2, strokeStyle:'#666' },
-    connector: [ "Straight"],
+    connector: [ "Flowchart"],
     isSource: true,
     isTarget: true,
     maxConnections: -1,
@@ -38,12 +12,20 @@ function makeSourceAndTarget(element , parents_list){
     beforeDrop:function(conn) { return allowParent(conn,parents_list); }
   };
   return jsPlumb.addEndpoint(element, sourceAndTargetEndPointAttributes);
-};
+}
 
 
 //Makes connections between instances
+function makeNodes(instances){
+    for(var key in instances){
+        var element = 'instance-'+key;
+        var instance = instances[key];
+        var parents_list = instance.configAttributes.parents_list;
+        makeSourceAndTarget(element,parents_list);
+    }
+}
+
 function makeConnections(instances){
-    var instanceEndpoints = {} ;
     for(var key in instances){
         var element = 'instance-'+key;
         //Make the dropped element draggable - Using JS plumb draggable
@@ -51,11 +33,12 @@ function makeConnections(instances){
 
         //Add connection endpoint to element
         var instance = instances[key];
-        var parents_list = instance.configAttributes.parents_list;
-        instanceEndpoints[key] = makeSourceAndTarget(element,parents_list);
         var parents = instance.parents ;
-        for(var i=0; i < parents.length; i++)
-          jsPlumb.connect({source: 'instance-'+parents[i], target: element,connector:[ "Straight"],anchor:"BottomLeft",endpoint: ["Dot", {radius: 4}] ,overlays:[[ "Arrow", { width:8, length:15}]]});
+        for(var i=0; i < parents.length; i++) {
+            sourceEndpoint = jsPlumb.selectEndpoints({source: 'instance-'+parents[i]}).get(0);
+            targetEndpoint = jsPlumb.selectEndpoints({target: element}).get(0);
+            jsPlumb.connect({source: sourceEndpoint, target: targetEndpoint});
+        }
     }
 }
 
@@ -66,31 +49,26 @@ function getParentDomIds(element){
   for(var i=0; i < connections.length; i++)
     parentDomIds.push(connections[i].source.attr('id'));
   return parentDomIds;
-};
+}
 
 function allowParent(conn, parents_list){
   var instanceOptions = $('#'+conn.sourceId).instance("option");
   var result = false;
   if(parents_list != null){
     var parents = parents_list.split(',');
-    for(var i=0; i<parents.length; i++){
-      if (parents[i] == instanceOptions.resourceType)
-        return true;
+    for(var i=0; i<parents.length; i++) {
+       if (parents[i] == instanceOptions.resourceType){
+           result = true;
+           break;
+       }
     }
   }
-  return result;
-};
+return result;
+}
 
 $(document).ready(function(){
   // bind click listener; delete connections on click
   jsPlumb.bind("click", function(conn) {
     jsPlumb.detach(conn);
-  });
-//modifying the default settings
-  jsPlumb.importDefaults({
-	PaintStyle : {
-		lineWidth:2,
-		strokeStyle: '#666'
-	}
   });
 });
