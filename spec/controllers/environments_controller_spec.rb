19f7db5ba7b4d75ef6bc180e2dc13feee0b89c78
Siva Prasad <sivaprasad@neevtech.com>
2013-01-25 12:20:03 +0530

    passing parameter 'project id' to the create and update test cases in environment_controller_spec

--- spec/controllers/environments_controller_spec.rb
+++ spec/controllers/environments_controller_spec.rb
@@ -2,35 +2,35 @@ require 'spec_helper'
 
 describe EnvironmentsController do
 
-=begin
-  describe "POST #create" do
-    def do_post
-      post :create, environment: {name: 'testEnvironment'}, instances: [{label:'WebServer', xpos: 10, ypos: 20, ami_id: 1},{label: 'MySQL', xpos: 5, ypos: 15}]
-    end
+  let(:user) { FactoryGirl.create(:user) }
+  let(:project) { FactoryGirl.create(:project) }
 
-    let(:user) { FactoryGirl.create(:user) }
+  describe "POST #create" do
+    def do_post(project_id)
+      post :create, project_id: project_id,environment: {name: 'testEnvironment'}, instances: [{label:'WebServer', xpos: 10, ypos: 20, instance_type_id:1,resource_type: 'Rails',config_attributes:{}},{label: 'MySQL', xpos: 5, ypos: 15, instance_type_id:1,resource_type: 'Mysql',config_attributes:{}}]
+    end    
 
     it "should increase the environment count by 1" do
       sign_in(user)
       expect do
-        do_post 
+        do_post(project.id)
       end.to change {Environment.count}.by(1)
     end
 
     it "should increase the instance count by 2" do
       sign_in(user)
       expect do
-        do_post 
+        do_post(project.id)
       end.to change {Instance.count}.by(2)
     end
   end
 
   describe "POST #update" do
-    def do_put(id)
-      put :update, id: id, instances: [{label:'WebServerEdited', xpos: 10, ypos: 20, ami_id: 1},{label: 'MySQLEdited', xpos: 5, ypos: 15}]
+    def do_put(project_id,id)
+      put :update, project_id: project_id, id: id, instances: [{label:'WebServerEdited', xpos: 10, ypos: 20, instance_type_id:1,resource_type: 'Rails',config_attributes:{}},{label: 'MySQLEdited', xpos: 5, ypos: 15, instance_type_id:1,resource_type: 'Mysql',config_attributes:{}}]
     end
 
-    let(:user) { FactoryGirl.create(:user) }
+    
     let(:environment) {FactoryGirl.create(:environment)}
     let(:ec2_instance) { FactoryGirl.create(:ec2_instance, environment: environment) }
     let(:rds_instance) { FactoryGirl.create(:rds_instance, environment: environment) }
@@ -42,7 +42,7 @@ describe EnvironmentsController do
     it "should not increase the environment count" do
       sign_in(user)
       expect do
-        do_put(environment.id)
+        do_put(project.id,environment.id)
       end.to change {Environment.count}.by(0)
     end
 
@@ -50,20 +50,19 @@ describe EnvironmentsController do
       sign_in(user)
       expect do
         environment.instances.count.should == 2
-        do_put(environment.id)
+        do_put(project.id,environment.id)
       end.to change {Instance.count}.by(0)
     end
 
     it "should update the instances" do
       sign_in(user)
       expect do
-        do_put(environment.id)
+        do_put(project.id,environment.id)
         environment.reload
         environment.instances.first.label.should == 'WebServerEdited'
         environment.instances.second.label.should == 'MySQLEdited'
       end.to change {Instance.count}.by(0)
     end
   end
-=end
 
 end
