3651619e13cdf65a4ebaa5a97e55247fa5ba77d0
Siva Prasad <sivaprasad@neevtech.com>
2013-02-01 14:28:05 +0530

    factories done for instance_type and region model and removed code in resource_type factory

---
+++ spec/factories/instance_types.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :instance_type , class: :instance_type do
+      name 'Micro'
+      api_name 'cache.t1.micro'
+    end
+  end
--- spec/factories/resource_types.rb
+++ spec/factories/resource_types.rb
@@ -4,16 +4,3 @@ FactoryGirl.define do
       resource_class "RDS"
     end
   end
-
-FactoryGirl.define do
-  factory :instance_type , class: :instance_type do
-      name 'Micro'
-      api_name 'cache.t1.micro'
-    end
-  end
-
-FactoryGirl.define do
-  factory :region , class: :region do
-      name 'us-east-1'      
-    end
-  end
